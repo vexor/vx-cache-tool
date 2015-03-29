818775bce0387f102be8840db2adc3a61f52219f
Pavel Argentov <argentoff@gmail.com>
2015-03-29 14:02:32 +0300

    Httpdiy enough for 'fetch' testing

--- httpdiy/server.go
+++ httpdiy/server.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"github.com/codegangsta/cli"
+	"io"
 	"log"
+	"net/http"
 )
 
 func runServer(c *cli.Context) {
-	log.Println("server: starting")
+	log.Println("server: starting at localhost:4242")
+
+	http.HandleFunc("/urls/r123456", fetchUrlHandler)
+	http.HandleFunc("/storage/fetch.tgz", fetchFileHandler)
+
+	log.Fatal(http.ListenAndServe("localhost:4242", nil))
+
 	log.Println("server: finishing")
+}
+
+func fetchUrlHandler(w http.ResponseWriter, req *http.Request) {
+	log.Println(req.Method, req.URL)
+	io.WriteString(w, "http://localhost/storage/fetch.tgz")
+}
+
+func fetchFileHandler(w http.ResponseWriter, req *http.Request) {
+	log.Println(req.Method, req.URL)
+	http.ServeFile(w, req, "files/fetch.tgz")
 }
